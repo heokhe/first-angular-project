fefb58fe4fcfa575e4cc4912b48e0b816ed78ec7
Hosein Khansari <hrk.x11@gmail.com>
2022-09-17 11:58:53 +0430

    Add tests for the toSymbol pipe

--- src/app/to-symbol.pipe.spec.ts
+++ src/app/to-symbol.pipe.spec.ts
@@ -1,8 +1,17 @@
 import { ToSymbolPipe } from './to-symbol.pipe';
 
 describe('ToSymbolPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToSymbolPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: ToSymbolPipe;
+
+  beforeEach(() => {
+    pipe = new ToSymbolPipe();
+  });
+
+  it('should convert common operations to symbols', () => {
+    expect(pipe.transform('add')).toEqual('+');
+    expect(pipe.transform('multiply')).toEqual('*');
+  });
+  it('should skip transforming other inputs', () => {
+    expect(pipe.transform('some other input')).toEqual('some other input');
   });
 });

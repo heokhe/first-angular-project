8f257dd01f3a5c7a017292436f9632087c776383
Hosein Khansari <hrk.x11@gmail.com>
2022-09-17 11:58:31 +0430

    Remove the routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToSymbolPipe } from './to-symbol.pipe';
@@ -13,7 +12,6 @@ import { ToSymbolPipe } from './to-symbol.pipe';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MatSnackBarModule,
   ],
